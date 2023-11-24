7ec055de551a12de5787434163d5a9b5ee8adae1
Andrey Bejarano <andreybejarano@gmail.com>
2023-11-24 02:12:35 +0200

    clase42

---
+++ clase42/src/routes/moviesRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const moviesController = require('../controllers/moviesController');
+
+router.get('/', moviesController.list);
+router.get('/new', moviesController.new);
+router.get('/recommended', moviesController.recomended);
+router.get('/detail/:id', moviesController.detail);
+//Rutas exigidas para la creación del CRUD
+router.get('/add', moviesController.add);
+router.post('/create', moviesController.create);
+router.get('/edit/:id', moviesController.edit);
+router.put('/update/:id', moviesController.update);
+router.get('/delete/:id', moviesController.delete);
+router.delete('/delete/:id', moviesController.destroy);
+
+router.post('/movies/buscar', moviesController.buscar);
+module.exports = router;
